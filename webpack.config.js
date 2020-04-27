728b65fc158a70630592567cec20636975e148fd
Mr.cat <fantasy.lian@163.com>
2020-04-27 23:54:13 +0800

    add config files

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+    entry: {
+        main: './main'
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        less: ExtractTextPlugin.extract ({
+                            use: ['css-loader', 'less-loader'],
+                            fallback: 'vue-style-loader'
+                        }),
+                        css: ExtractTextPlugin.extract({
+                            use: ['css-loader','less-loader'],
+                            fallback: 'vue-style-loader'
+                        })
+                    }
+                }
+            },
+            {
+                test: /\.less/,
+                use: ExtractTextPlugin.extract({
+                    use: ['less-loader'],
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader',
+                    fallback: 'style-loader'
+                })
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=1024'
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: '[name].css',
+            allChunks: true
+        })
+    ]
+};
+
+module.exports = config;
