06769cefd89c9a157deb786d9fedebc618f318f0
Mr.cat <fantasy.lian@163.com>
2020-05-02 23:44:19 +0800

    modify some config

--- components/my-home.vue
+++ components/my-home.vue
@@ -3,7 +3,7 @@
         <div class="overlay">
             <div class="col home-title jiji">
                 <h1>LIAN JUN</h1>
-                <h2>PC / Phone / Hybrid App</h2>
+                <h2>WEB/ H5 / Hybrid App / Mini Apps</h2>
                 <a href="./myResume.pdf" target="_blank" class="but">下载简历</a>
             </div>
         </div>
