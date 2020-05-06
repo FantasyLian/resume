54c7bfdbaf912088f204f67604573175b39a26d8
Mr.cat <fantasy.lian@163.com>
2020-05-06 23:44:45 +0800

    modify main

--- main.js
+++ main.js
@@ -2,9 +2,8 @@
 
 import Vue from 'vue';
 import App from './app.vue';
-import './src/assets/js/skrollr.js'
-import './src/assets/js/resume.js'
-import './src/assets/css/normalize.min.css'
+import './src/assets/js/resume.js';
+import './src/assets/css/normalize.min.css';
 import './src/assets/css/style.css';
 
 new  Vue({
